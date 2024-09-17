0abf8b9089141ba485b79efb05f8bd6e5a5fa930
michallmu <michalmu123@gmail.com>
2024-09-17 22:48:43 +0300

    =first commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,58 @@
+plugins {
+    //alias(libs.plugins.android.application)
+    id("com.android.application")
+
+        // Add the Google services Gradle plugin
+    id("com.google.gms.google-services")
+
+
+
+}
+
+android {
+    namespace = "triviyou.michal.com"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "triviyou.michal.com"
+        minSdk = 26
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+dependencies {
+
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    implementation(libs.firebase.auth)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+    // Import the Firebase BoM
+    implementation(platform("com.google.firebase:firebase-bom:33.3.0"))
+    // TODO: Add the dependencies for Firebase products you want to use
+    // When using the BoM, don't specify versions in Firebase dependencies
+    implementation("com.google.firebase:firebase-analytics")
+
+    implementation ("com.google.firebase:firebase-auth:23.0.0")
+}
